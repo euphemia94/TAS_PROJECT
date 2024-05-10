af9d2e66379c86bc3db3b6f00a5632a099da4bc6
euphemia94 <euphemiauc94@gmail.com>
2024-05-10 14:28:59 +0100

    add module 4c

---
+++ Module4C/Task/Task14.py
@@ -0,0 +1,7 @@
+# Declare a string variable with any value
+# Print out the string in the upper case form
+
+string = "test automation simplified"
+# to convert to upper case
+upper_value = string.upper()
+print("upper:", upper_value)
