02e018b19e0fab4b7527728a8594df038552176e
euphemia94 <euphemiauc94@gmail.com>
2024-06-28 12:58:20 +0100

    add project

---
+++ Module4C/appium_mobile/saucedemo.py
@@ -0,0 +1,24 @@
+import time
+
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def main():
+    desired_caps = {
+        "deviceName": "Android Emulator",
+        "platformName": "Android",
+        "platformVersion": "6",
+        "app": "C:/Users/USER/Downloads/Android-MyDemoAppRN.1.1.0.build-226.apk",
+        "appPackage": "com.saucelabs.mydemoapp.rn",
+        "appActivity": ".activities.Music",
+        "noSign": True,
+        #"androidInstallTimeout": 120000  # Increase install timeout to 2 minutes (120000 ms)
+    }
+    driver = webdriver.Remote(command_executor="http://127.0.0.1:4723/wd/hub", desired_capabilities=desired_caps)
+    time.sleep(5)
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
